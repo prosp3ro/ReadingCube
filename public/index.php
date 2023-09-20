4c111d02d15a069673538222ac1d9e1f47270fc4
prospero <g0lden_vo1d@protonmail.com>
2023-09-20 10:39:15 +0200

    session handling

--- public/index.php
+++ public/index.php
@@ -22,7 +22,28 @@ require_once(ROOT . "/vendor/autoload.php");
 
 ini_set('session.use_only_cookies', 1);
 ini_set('session.use_strict_mode', 1);
+
+session_set_cookie_params([
+    "lifetime" => 1800, // 30 minutes
+    "domain" => $config["app"]["domain"] ?? "localhost",
+    "path" => "/",
+    "secure" => true,
+    "httponly" => true
+]);
+
 session_start();
+
+if (!isset($_SESSION["last_regeneration"])) {
+    session_regenerate_id(true);
+    $_SESSION["last_regeneration"] = time();
+} else {
+    $interval = 60 * 30; // 30 minutes
+
+    if (time() - $_SESSION["last_regeneration"] >= $interval) {
+        session_regenerate_id(true);
+        $_SESSION["last_regeneration"] = time();
+    }
+}
 
 try {
     require_once(ROOT . "/routes/web.php");
