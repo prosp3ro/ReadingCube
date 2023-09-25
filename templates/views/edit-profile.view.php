e13f7ee2d0ee04e059738317adcaf6f51383d367
prospero <g0lden_vo1d@protonmail.com>
2023-09-25 15:48:34 +0200

    user settings page

--- templates/views/edit-profile.view.php
+++ templates/views/edit-profile.view.php
@@ -1,38 +1,40 @@
 <?php require_once(PARTIALS . "/head.view.php"); ?>
 <?php require_once(PARTIALS . "/header.view.php"); ?>
 
-<div class="container rounded bg-white mt-5 mb-5">
-    <h2 class="ms-2 mt-2">Edit profile</h2>
+<div class="container">
+    <h2 class="my-5">Edit profile</h2>
 
     <div class="row">
-        <!-- <div class="col-md-5 border-right"> -->
+        <!-- <div class="col-md-5 border-right"> -->
         <!--     <div class="d-flex flex-column align-items-center text-center p-3 py-2"> -->
         <!--         <img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"> -->
         <!--         <span class="font-weight-bold">username</span> -->
         <!--         <span class="text-black-50">email</span><span> </span> -->
         <!--     </div> -->
         <!-- </div> -->
-        <div class="col-md-7 border-right">
-            <div class="p-3 py-3">
-                <div class="row mt-3">
-                    <div>
-                        <label class="labels">Mobile Number</label>
-                        <input type="text" class="form-control" placeholder="enter phone number" value="">
-                    </div>
-                    <div>
-                        <label class="labels">Address Line 1</label>
-                        <input type="text" class="form-control" placeholder="enter address line 1" value="">
-                    </div>
-                    <div>
-                        <label class="labels">Address Line 2</label>
-                        <input type="text" class="form-control" placeholder="enter address line 2" value="">
 
-                    </div>
+        <div class="w-25">
+            <form action="/register" method="post" id="register">
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control" name="username" id="username" required>
                 </div>
-                <div class="mt-3">
-                    <button class="btn btn-primary profile-button" type="button">Save Profile</button>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email address</label>
+                    <input type="email" class="form-control" name="email" id="email" required>
+                    <div class="form-text">We'll never share your email with anyone else.</div>
                 </div>
-            </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password" id="password" required>
+                </div>
+                <div class="mb-3">
+                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" name="password_confirmation" id="password_confirmation" required>
+                </div>
+                <div class="g-recaptcha" data-sitekey="<?= GOOGLE_RECAPTCHA_SITE_KEY ?>"></div>
+                <button type="submit" class="mt-2 btn btn-primary">Submit</button>
+            </form>
         </div>
     </div>
 </div>
