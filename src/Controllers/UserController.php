33c5b28bfa419cd1b485cd1495b6f0fb43ff17a3
prospero <g0lden_vo1d@protonmail.com>
2023-09-26 11:42:44 +0200

    edit user page

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Src\Controllers;
 
+use PDOException;
 use Src\Exceptions\DatabaseQueryException;
 use Src\Models\DB;
 use Src\View;
@@ -47,10 +48,11 @@ class UserController
 
     public function editProfileData(object $captcha)
     {
-        $newUsername = $_POST['newUsername'] ?? null;
-        $newEmail = $_POST['newEmail'] ?? null;
-        $password = $_POST['password'];
-        $captchaResponseKey = $_POST['g-recaptcha-response'];
+        $newUsername = $_POST["newUsername"] ?? null;
+        $newEmail = $_POST["newEmail"] ?? null;
+        $password = $_POST["password"];
+        $captchaResponseKey = $_POST["g-recaptcha-response"];
+        $sessionUserId = $_SESSION["user_id"];
 
         if (empty($password)) {
             exit("Password is required.");
@@ -61,12 +63,40 @@ class UserController
         }
 
         if ($newUsername && $newEmail) {
+            // validate username
             if (!preg_match('/^[a-zA-Z0-9]{5,}$/', $newUsername)) {
                 exit("Invalid username format. Please use only letters and numbers, and ensure it's at least 5 characters long.");
             }
 
+            // validate email
             if (!filter_var($newEmail, FILTER_VALIDATE_EMAIL)) {
                 exit("Email has invalid format");
+            }
+
+            // verify password
+            $sql = "SELECT * FROM users WHERE id = ?";
+            $statement = $this->db->prepare($sql);
+
+            try {
+                $statement->execute([$sessionUserId]);
+                $user = $statement->fetch();
+            } catch (PDOException $exception) {
+                throw new DatabaseQueryException($exception->getMessage());
+            }
+
+            if (!$user || !password_verify($password, $user['password'])) {
+                exit("...");
+            }
+
+            // update username and email in database
+            $updateSql = "UPDATE users SET username = ?, email = ? WHERE id = ?";
+            $statement = $this->db->prepare($updateSql);
+
+            try {
+                $statement->execute([$newUsername, $newEmail, $sessionUserId]);
+            } catch (Throwable $exception) {
+                throw new DatabaseQueryException($exception->getMessage());
+                exit();
             }
         } else if ($newEmail) {
             if (!filter_var($newEmail, FILTER_VALIDATE_EMAIL)) {
