2f9dede92fbb9f6d50bd7ae733f51d87cb047ca3
prospero <g0lden_vo1d@protonmail.com>
2023-09-28 14:35:45 +0200

    change error_log message

--- public/index.php
+++ public/index.php
@@ -53,7 +53,12 @@ try {
     require_once(ROOT . "/routes/web.php");
 } catch (Throwable $exception) {
     $exceptionClassName = get_class($exception);
-    $errorLogMessage = date('Y-m-d H:i:s') . PHP_EOL . "Exception: {$exceptionClassName}" . PHP_EOL . "Message: {$exception->getMessage()}" .  PHP_EOL . "File: {$exception->getFile()}" . PHP_EOL . "Line: {$exception->getLine()}" . PHP_EOL . PHP_EOL;
+    $errorLogMessage = date('Y-m-d H:i:s') . PHP_EOL .
+        "Exception: {$exceptionClassName}" . PHP_EOL .
+        "Message: {$exception->getMessage()}" . PHP_EOL .
+        "File: {$exception->getFile()}" . PHP_EOL .
+        "Line: {$exception->getLine()}" . PHP_EOL . PHP_EOL;
+
     error_log($errorLogMessage, 3, ROOT . "/logs/error.log");
 
     if (function_exists("showException")) {
