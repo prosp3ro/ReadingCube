67e7eebab13cc03305cdd7e4b6b87a09764aeec6
prospero <g0lden_vo1d@protonmail.com>
2023-10-02 17:07:09 +0200

    Use 'Comfortaa' font

--- templates/partials/head.view.php
+++ templates/partials/head.view.php
@@ -9,6 +9,9 @@
     <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&display=swap" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
 </head>
 
