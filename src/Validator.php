db291d200f3d304ce964420e2210f3c9449ca315
prospero <g0lden_vo1d@protonmail.com>
2023-10-04 12:05:36 +0200

    refactor Validator class

--- src/Validator.php
+++ src/Validator.php
@@ -13,9 +13,12 @@ class Validator
 
     public function validate(array $args)
     {
-        if (array_key_exists("username", $args)) {
-            $username = $args["username"];
+        $username = $args["username"] ?? "";
+        $email = $args["email"] ?? "";
+        $password = $args["password"] ?? "";
+        $passwordConfirmation = $args["password_confirmation"] ?? "";
 
+        if (!empty($username)) {
             if (!preg_match($this->usernameRegex, $username)) {
                 exit("Invalid username format. Please use only letters and numbers, and ensure it's at least 5 characters long.");
             }
@@ -25,9 +28,7 @@ class Validator
             }
         }
 
-        if (array_key_exists("email", $args)) {
-            $email = $args["email"];
-
+        if (!empty($email)) {
             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
                 exit("Email has invalid format.");
             }
@@ -37,13 +38,10 @@ class Validator
             }
         }
 
-        if (array_key_exists("password", $args)) {
-            if (!array_key_exists("password_confirmation", $args)) {
+        if (!empty($password)) {
+            if (empty($passwordConfirmation)) {
                 throw new \Exception("password_confirmation key does not exist in validate arguments.");
             }
-
-            $password = $args["password"];
-            $passwordConfirmation = $args["password_confirmation"];
 
             if (!preg_match($this->passwordRegex, $password)) {
                 exit("Password must be at least 8 characters and contain at least 1 uppercase letter, 1 lowercase letter, and 1 number.");
