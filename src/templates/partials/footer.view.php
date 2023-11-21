28e168fbb95fb7b360f9e2dc8a59b4b2a6a59d44
prosp3ro <g0lden_vo1d@protonmail.com>
2023-11-21 09:25:08 +0100

    refactor: update project structure

--- src/templates/partials/footer.view.php
+++
@@ -1,8 +0,0 @@
-<footer class="text-center text-white bg-dark">
-  <!-- <div class="container p-4"></div> -->
-
-  <div class="p-3">
-    © 2023 Copyright:
-    <a class="text-white" href="/"><?= APP_NAME ?></a>
-  </div>
-</footer>
